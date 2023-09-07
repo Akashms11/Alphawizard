ee3fb8d2854e803cd9f1559b2520f31c8d56e226
Akashms11 <akashms11.as@gmail.com>
2023-09-07 19:08:15 +0530

    "Fibonacci Number Series"

---
+++ src/main/java/com/subs/alphawizard/Algos/FibonacciRecursion.java
@@ -0,0 +1,21 @@
+package com.subs.alphawizard.Algos;
+
+import java.util.Scanner;
+//Recursive approach to print Fibonacci series
+public class FibonacciRecursion {
+
+    static int fibonacci(int n){
+        if(n<=1)
+            return n;
+        else return fibonacci(n-1)+fibonacci(n-2);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a range number: ");
+        int n = sc.nextInt();
+        for (int i=0;i<n;i++){
+          System.out.print(" "+fibonacci(i));
+        }
+    }
+}
